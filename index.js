634f146ae27fb8f8b53aad43df6b568eedc31727
jainpranav1 <pranavji@tamu.edu>
2022-07-12 21:32:11 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const puppeteer = require("puppeteer");
+
+let person = "Tom Cruise";
+
+let prompts = [
+  `who is ${person}`,
+  `who did ${person}`,
+  `who would ${person}`,
+  `who is ${person}`,
+  `how is ${person}`,
+  `how did ${person}`,
+  `what does ${person}`,
+  `what was ${person}`,
+  `what is ${person}`,
+  `who is ${person}`,
+  `who did ${person}`,
+  `who would ${person}`,
+  `can ${person}`,
+  `does ${person}`,
+];
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://google.com");
+
+  for (let i = 0; i < prompts.length; ++i) {
+    await page.goto("https://google.com");
+    await page.type("[aria-label='Search']", prompts[i]);
+    await page.waitForTimeout(1000);
+    await page.screenshot({ path: `images/${i}.png` });
+  }
+
+  await browser.close();
+})();
